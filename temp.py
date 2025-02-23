be69131573b0ac448144d6c2fc06cf7b0237c171
wu <2450402591@qq.com>
2025-02-23 10:51:18 +0800

    1

--- temp.py
+++ temp.py
@@ -11,19 +11,25 @@ def get_workflow_file_history(repo_full_name, file_path, api_token):
     :param api_token: GitHub API 的访问令牌
     :return: 提交历史记录列表
     """
-    api_url = f"https://api.github.com/repos/{repo_full_name}/commits"
-    params = {"path": file_path}
+    api_url = f"https://api.github.com/repos/{repo_full_name}/actions/runs"
+    #params = {"path": file_path}
     headers = {
         "Authorization": f"token {api_token}",
         "Accept": "application/vnd.github.v3+json"
     }
 
-    response = requests.get(api_url, headers=headers, params=params)
+    response = requests.get(api_url, headers=headers)
     
-    if response.status_code == 200:
-        return response.json()  # 返回提交历史记录
-    else:
-        raise Exception(f"错误：{response.status_code}, {response.text}")
+    if response.status_code != 200:
+        print(f"Failed to fetch runs for : {response.status_code}")
+        return []
+    
+    data = response.json()
+    for run in data.get('workflow_runs', []):
+        if run['status'] == 'completed' and run['conclusion'] == 'success':
+            print("222")
+            break
+
 
 
 
@@ -34,13 +40,13 @@ def get_workflow_file_history(repo_full_name, file_path, api_token):
 if __name__ == "__main__":
     # 从 CSV 文件中读取仓库，并获取每个仓库的 workflow 文件提交历史
     api_token = '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'
-    repo_full_name = 'pythological/kanren'
+    repo_full_name = 'jongracecox/anybadge'
     workflow_file_path = '.travis.yml'
     commits = get_workflow_file_history(repo_full_name, workflow_file_path, api_token)
-    for c in commits:
-        b = c['commit']['message']
-        if "Migrate" in b or 'Remove' in b:
-            print(b)
-        #    print(c['commit']['author']["date"])
-        #    break
-        #print(b)
+
+    
+    '''
+    for run in data.get('workflow_runs', []):
+        if run['status'] == 'completed' and run['conclusion'] == 'success':
+            print('sss')
+    '''
