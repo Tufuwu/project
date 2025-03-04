2ad6963bda502829ba9925bbcf13959bd3c4fc77
wu <wu@.com>
2025-03-04 17:14:22 +0800

    1

--- temp1.py
+++ temp1.py
@@ -32,24 +32,24 @@ if "origin" in check_remote.stdout:
 subprocess.run(["git", "remote", "add", "origin", github_repo_url], check=True)
 '''
 # 读取CSV文件，获取文件路径
-count = 106
+count = 281
 
 df = pd.read_csv(csv_file_path)
 for index, row in df.iterrows():
     repo_full_name = row['full_name']
     base_download_path =f"D:/vscode/3/project/data1/{repo_full_name}/travis.yml"
-    '''
+    
     copy_files(base_download_path,local_file)
     os.chdir(local_directory)
     push_repositories('Update files from CSV')
     add_comments(api_token)
     time.sleep(90)
-    '''
+    
     commits = gh.get_request_commit('Tufuwu/myproject',api_token,str(count))
     data = commits.json()
     for c in data:
         output_dir = os.path.join(parent_dir, repo_full_name)
         fo.split_and_save_diffs(gh.get_commit_diff(c['url'],api_token), output_dir)
-    break
+    #break
     count+=1
 
