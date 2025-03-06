f3f31ff73e22e1be5b778d287fe74e056569702e
wu <wu@.com>
2025-03-06 18:31:56 +0800

    1

--- hunyuan-lite.py
+++ hunyuan-lite.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
 from utils import prompt_constructor
+import os
+import pandas as pd
 
 api_key = "sk-pD0ToEy2oNONeewKSIoWMYAEteBN6QCQ0mGTwIoLPCLL97ws"
 
@@ -8,54 +10,52 @@ client = OpenAI(
     base_url="https://api.hunyuan.cloud.tencent.com/v1", # 混元 endpoint
 )
 
-s1 = '''language: java
-sudo: false
-install: true
-
-jdk:
-  - openjdk8
-  - openjdk11
-  - openjdk12
-  #  - openjdk13
-  - openjdk-ea
-  #  - oraclejdk8
-  - oraclejdk11
-
-matrix:
-  allow_failures:
-    - jdk: openjdk-ea
-
-env:
-  - AWS_TEST_BUCKET=s3-tests.s3-eu-west-1.amazonaws.com
-  - AWS_TEST_BUCKET=s3.eu-central-1.amazonaws.com/s3-tests-2
-  - AWS_TEST_BUCKET=s3.amazonaws.com/s3-tests-3
-
-after_success:
-  - mvn -V -B -e jacoco:report
-  - bash <(curl -s https://codecov.io/bash)
-
-
-'''
-write_migration_template = prompt_constructor('h1','h2','h3','h6','h5')
-prompt =  write_migration_template.format(sourcelang = 'Travis CI',targetlang = 'Github Action',sourcefile = '1',sourcefile_content =s1 ,guidelines=None)
+def read_file(file_path):
+    try:
+        with open(file_path,"r",encoding= 'utf-8') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 
-completion = client.chat.completions.create(
-    model="hunyuan-pro",
-    messages=[
-        {
-            "role": "user",
-            "content": prompt,
+
+csv_file = "D:/vscode/3/project/python-csv/temp.csv"
+df =pd.read_csv(csv_file)
+for index,row in df.iterrows():
+    repo_full_name = row['full_name']
+    file_path = f"D:/vscode/3/project/data1/{repo_full_name}/travis.yml"
+    s1 = read_file(file_path)
+    write_migration_template = prompt_constructor('1','2','3')
+    prompt =  write_migration_template.format(sourcelang = 'Travis CI',targetlang = 'Github Action',sourcefile_content =s1 )
+
+
+
+    completion = client.chat.completions.create(
+        model="hunyuan-pro",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        extra_body={
+            "enable_enhancement": False, # <- 自定义参数
         },
-    ],
-    extra_body={
-        "enable_enhancement": False, # <- 自定义参数
-    },
-)
+    )
 
 
 
 
-reponse = completion.choices[0].message.content
-print(reponse)
+    reponse = completion.choices[0].message.content
+
+    file_name = f"gpt.yml"
+    output_dir = f"D:/vscode/3/project/data1/{repo_full_name}"
+    output_path = os.path.join(output_dir, file_name)
+    with open(output_path, 'w') as f:
+        f.write(reponse)
+    print(f"Saved diff to {output_dir}")
+    
