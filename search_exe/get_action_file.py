4db944b03c718c775919640735e81af026e8ca35
wu <2450402591@qq.com>
2025-03-31 19:10:21 +0800

    1

--- search_exe/get_action_file.py
+++ search_exe/get_action_file.py
@@ -3,16 +3,17 @@ import subprocess
 from utils import file_operate,get_history
 
 
-csv_file = 'D:/vscode/3/project/repositories/fix_time.csv'
+csv_file = 'D:/vscode/3/project/python-csv/final_csv/fix_time.csv'
 df = pd.read_csv(csv_file)
 fo = file_operate()
 for index, row in df.iterrows():
     repo_full_name = row['full_name']
     new_data ={'full_name':row['full_name'],'travisredate':row['travisredate'],'file_name':row['file_name'],'commit_sha':row['commit_sha'],'new_commits_sha':0,'commits_times':0}
+    file_name = row['file_name']
     version = row['new_commits_sha']
     try:
         
-        repo_path = f'D:/vscode/repositories/{repo_full_name}'
+        repo_path = f'D:/vscode/repo/{repo_full_name}'
         subprocess.run(["git", "-C", repo_path, "fetch", "--all"], check=True)
         
         # 切换到指定版本
@@ -20,8 +21,8 @@ for index, row in df.iterrows():
         
         
         print(f"Successfully switched to {version}")
-        file_path = f'D:/vscode/repositories/{repo_full_name}/.github/workflows/{new_data}'
-        target_directory = f'D:/vscode/3/project/data1/{repo_full_name}/action.yml'
+        file_path = f'D:/vscode/repo/{repo_full_name}/.github/workflows/{file_name}'
+        target_directory = f'D:/vscode/3/project/data2/{repo_full_name}/action.yml'
         with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
         with open(target_directory, "w", encoding="utf-8") as f:
