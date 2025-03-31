f198516d6f56655e3366f143ff7e0696b808b251
wu <2450402591@qq.com>
2025-03-31 19:10:00 +0800

    1

--- search_exe/get_action_file.py
+++ search_exe/get_action_file.py
@@ -1,15 +1,25 @@
 import pandas as pd
-import os
+import subprocess
 from utils import file_operate,get_history
 
 
-csv_file = 'D:/vscode/3/project/repositories/fix_time_2.csv'
+csv_file = 'D:/vscode/3/project/repositories/fix_time.csv'
 df = pd.read_csv(csv_file)
+fo = file_operate()
 for index, row in df.iterrows():
     repo_full_name = row['full_name']
-    new_data =row['file_name']
-
+    new_data ={'full_name':row['full_name'],'travisredate':row['travisredate'],'file_name':row['file_name'],'commit_sha':row['commit_sha'],'new_commits_sha':0,'commits_times':0}
+    version = row['new_commits_sha']
     try:
+        
+        repo_path = f'D:/vscode/repositories/{repo_full_name}'
+        subprocess.run(["git", "-C", repo_path, "fetch", "--all"], check=True)
+        
+        # 切换到指定版本
+        subprocess.run(["git", "-C", repo_path, "checkout", version], check=True)
+        
+        
+        print(f"Successfully switched to {version}")
         file_path = f'D:/vscode/repositories/{repo_full_name}/.github/workflows/{new_data}'
         target_directory = f'D:/vscode/3/project/data1/{repo_full_name}/action.yml'
         with open(file_path, "r", encoding="utf-8") as f:
@@ -18,7 +28,9 @@ for index, row in df.iterrows():
             f.writelines(lines)
  
     except Exception as e:
-        print(f"无法获取 {repo_full_name} 的 workflow 文件历史: {e}")
+        file_path = "D:/vscode/3/project/search_exe/errer_file.csv"
+        fo.write_file_in(file_path,new_data)
+
 
 
 
