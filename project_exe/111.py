879804f584f1c15dbf5f1e7a612f60d5203cfc17
wu <wu@.com>
2025-04-09 13:57:55 +0800

    1

--- project_exe/111.py
+++ project_exe/111.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import os
-from search_exe import get_history,file_operate
 
-csv_file = "D:/vscode/3/project/python-csv/final_csv/repo1.csv"  
-df = pd.read_csv(csv_file)
-fo = file_operate()
-for index, row in df.iterrows():
-    repo_full_name = row['full_name']
-    new_data ={'full_name':row['full_name'],'commits':row['commits'],'releases':row['releases'],'forks':row['forks'],'stargazers':row['stargazers'],'size':row['size'],'createdAt':row['createdAt'],'pushedAt':row['pushedAt'],'updatedAt':row['updatedAt'],'lastCommit':row['lastCommit'],'travisredate':row['travisredate'],'commit_sha':row['commit_sha'],'file_name':row['file_name']}
+def write_file_in(file_path,target_directory):
+    os.makedirs(os.path.dirname(target_directory), exist_ok=True)
+    file_name = 'gpt-4o.yml'
+    file_path +=file_name
+    target_directory += file_name
+    with open(file_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    
+    with open(target_directory, "w", encoding="utf-8") as f:
+        f.writelines(lines)
 
-    repo_path = f'd:/vscode/repo/{repo_full_name}'
-    if os.path.exists(repo_path):
-        pass
-    else:
-        file_path = 'D:/vscode/3/project/python-csv/target3.csv'
-        fo.write_file_in(file_path,new_data)
+if __name__ =='__main__':
+    csv_file_path = "D:/vscode/3/project/project_exe/python_csv/final_csv/now_can_run.csv"
+    df = pd.read_csv(csv_file_path)
+
+    for index, row in df.iterrows():
+        repo_full_name = row['full_name']
+        file_path = f'D:/vscode/3/project/data/{repo_full_name}/'
+        target_directory = f'D:/vscode/3/project/test_data/{repo_full_name}/'
+        write_file_in(file_path,target_directory)
---
+++ project_exe/search_exe/get_commit_times.py
@@ -0,0 +1,114 @@
+from utils import get_history,file_operate
+import pandas as pd
+from datetime import datetime, timedelta
+import re
+from collections import Counter
+
+def get_flie_name(file):
+    
+    file_len = (len(file)-52)//2 + 20
+    pre_file = file[:len(file)-file_len-1]
+    pre_name = pre_file.split('/')[-1]
+    lat_name = file.split('/')[-1]
+    return pre_name,lat_name
+
+def process_repos_from_csv(csv_file, api_token):
+    """
+    读取 CSV 文件中的 GitHub 仓库，并获取每个仓库中 workflow 文件的提交历史
+    :param csv_file: 包含 GitHub 仓库信息的 CSV 文件
+    :param api_token: GitHub API 的访问令牌
+    """
+    df = pd.read_csv(csv_file)
+    for index, row in df.iterrows():
+        repo_full_name = row['full_name']
+        #repo_full_name = 'nextstrain/ncov' 
+        file_name = row['file_name']
+        #file_name = 'ci.yaml'
+        file_commit_date = row['travisredate']
+        #file_commit_date = '2022-04-28T20:05:14Z'
+        new_data ={'full_name':row['full_name'],'travisredate':row['travisredate'],'file_name':row['file_name'],'commit_sha':row['commit_sha'],'new_commits_sha':0,'commits_times':0}
+        print(index)
+
+        try:
+            # 假设 workflow 文件在 `.github/workflows/` 下
+            workflow_file_path = f'.github/workflows/'
+            print(f"正在获取仓库 {repo_full_name} 的 workflow 文件历史...")
+            
+            # 获取文件历史
+            gh = get_history()
+            fo = file_operate()
+            commits = gh.get_workflow_file_history(repo_full_name, workflow_file_path, api_token)
+            commits.reverse() 
+            fix_times = Counter()
+            for c in commits:
+                now_date = c['commit']['author']['date']
+
+   
+                date_format = "%Y-%m-%dT%H:%M:%SZ"
+                date1 = datetime.strptime(file_commit_date, date_format)
+                date2 = datetime.strptime(now_date, date_format)
+                seven_days_later = date1 + timedelta(days=7)
+                if date2 <= seven_days_later:
+                    diff_content = gh.get_commit_diff(c['url'],api_token)
+                    diff_pattern = re.compile(r'(diff --git a/[\S]+ b/[\S]+)')
+                    
+                    # 根据匹配到的 diff 开头分割 diff 内容
+                    diffs = diff_pattern.split(diff_content)
+                    diffs = diffs[1:]
+
+                    for i in range(0, len(diffs), 2):
+                        if re.search('workflows',diffs[i]):
+                            pre_name,lat_name = get_flie_name(diffs[i])
+
+                            #print(pre_name,lat_name)
+                            #print(len(pre_name),len(lat_name))
+                            if pre_name ==lat_name:
+                                fix_times[pre_name] += 1
+                                if pre_name ==file_name:
+                                    new_data['new_commits_sha'] = c['sha']
+                                
+                            else:
+                                if pre_name ==file_name:
+                                    file_name = lat_name
+                                    fix_times[lat_name] += fix_times[pre_name]
+                                    fix_times[pre_name] += 1
+                                    fix_times[lat_name] += 1
+                                    new_data['new_commits_sha'] = c['sha']
+                                    new_data['file_name'] = file_name
+                                    
+                                elif lat_name ==file_name:
+                                    fix_times[lat_name] += fix_times[pre_name]
+                                    fix_times[pre_name] += 1
+                                    fix_times[lat_name] += 1
+                                    new_data['new_commits_sha'] = c['sha']
+                                    
+                                else:
+                                    fix_times[pre_name] += 1
+                                    fix_times[lat_name] += 1
+                            #print(file_name)      
+                            #print('11',fix_times[file_name])
+                else:
+                    break
+            
+            
+            new_data['commits_times'] = fix_times[file_name] 
+            file_path = "D:/vscode/3/project/python_csv/final_csv/fix_time.csv"
+            fo.write_file_in(file_path,new_data)
+
+
+
+                                    
+        except Exception as e:
+            file_path = "D:/vscode/3/project/search_exe/errer_file.csv"
+            fo.write_file_in(file_path,new_data)
+        
+        
+
+
+
+if __name__ =="__main__":
+
+    api_token = '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'
+    csv_file = "D:/vscode/3/project/python_csv/final_csv/repo.csv"
+    
+    process_repos_from_csv(csv_file,api_token)
